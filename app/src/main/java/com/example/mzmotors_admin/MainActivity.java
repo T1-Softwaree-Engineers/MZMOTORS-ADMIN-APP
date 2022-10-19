74d5fb8ad2efd4021a681891d33b73ddf7fdb27c
Jk99z2 <eochoa11@ucol.mx>
2022-10-19 12:32:31 -0500

    Intento de login con MySQL, PHP y Android

--- app/src/main/java/com/example/mzmotors_admin/MainActivity.java
+++ app/src/main/java/com/example/mzmotors_admin/MainActivity.java
@@ -2,18 +2,35 @@ package com.example.mzmotors_admin;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
     Intent i;
     Button btnlogin;
+    EditText editEmail, editPassword;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         try {
             Thread.sleep(2000);
         } catch (InterruptedException e) {
@@ -22,18 +39,53 @@ public class MainActivity extends AppCompatActivity {
         setTheme(R.style.Theme_MZMOTORSADMIN);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editEmail = findViewById(R.id.emailUser);
+        editPassword = findViewById(R.id.passwordUser);
 
 
-        btnlogin = (Button) findViewById(R.id.btnlogin);
+        btnlogin = findViewById(R.id.btnlogin);
 
         btnlogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-                i = new Intent(MainActivity.this,HomeAdmin.class);
-                startActivity(i);
+                validarUsuario("http://localhost/validation.php");
             }
         });
 
+    }
+
+    private void validarUsuario(String URL)
+    {
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+        @Override
+        public void onResponse(String response) {
+            if(!response.isEmpty())
+            {
+                Intent intent = new Intent(getApplicationContext(), HomeAdmin.class);
+                startActivity(intent);
+            }
+            else
+            {
+                Toast.makeText(MainActivity.this, "Email o contraseña incorrectos", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }, new Response.ErrorListener() {
+        @Override
+        public void onErrorResponse(VolleyError error) {
+            Toast.makeText(MainActivity.this, error.toString(), Toast.LENGTH_LONG).show();
+            String text = error.toString();
+            Log.e("error",text);
+        }
+    }){
+        @Override
+        protected Map<String, String> getParams() throws AuthFailureError {
+            Map<String, String> parametros = new HashMap<String, String>();
+            parametros.put("email", editEmail.getText().toString());
+            parametros.put("password", editPassword.getText().toString());
+            return parametros;
+        }
+    };
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
     }
 }
